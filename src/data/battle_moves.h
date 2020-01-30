2c79f36e3c45d04c8c6420ccb5ccaca21f87d559
LOuroboros <lunosouroboros@gmail.com>
2020-01-30 10:23:11 -0300

    Oops. I skipped Drain Punch by accident.

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -5735,7 +5735,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .secondaryEffectChance = 0,
         .target = MOVE_TARGET_SELECTED,
         .priority = 0,
-        .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGSROCK_AFFECTED,
+        .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGSROCK_AFFECTED | FLAG_IRON_FIST_AFFECTED,
         .category = MOVE_CATEGORY_PHYSICAL,
     },
 
